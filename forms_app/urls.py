741fbc37d9164d9552e861e230ada0a139f4dd91
VovkaST <vovka.steel911@mail.ru>
2020-11-22 17:58:42 +0300

    Изменение формы

--- forms_app/urls.py
+++ forms_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from forms_app.views import FormsListView, FormCreateView, FormDetailView
+from forms_app.views import FormsListView, FormCreateView, FormUpdateView
 
 urlpatterns = [
     path('', FormsListView.as_view(), name='forms_list'),
     path('create/', FormCreateView.as_view(), name='form_create'),
-    path('<slug>', FormDetailView.as_view(), name='form_view'),
+    path('edit/<slug>', FormUpdateView.as_view(), name='form_edit'),
 ]
--- forms_app/views.py
+++ forms_app/views.py
@@ -5,31 +5,23 @@ from forms_app.forms import FormsCreateForm, FieldFormSet
 from forms_app.models import Forms
 
 
-class FormsListView(generic.ListView):
-    model = Forms
-
-
-class FormDetailView(generic.DetailView):
-    model = Forms
-
-
-class FormCreateView(generic.CreateView):
-    template_name = 'forms_app/form_create.html'
-    form_class = FormsCreateForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if 'formset' not in context:
-            context.update({
-                'formset': FieldFormSet(prefix='field')
-            })
-        return context
-
+class DynamicFormMixin(object):
     def get_success_url(self):
         return reverse('forms_list')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        initial = self.object.form_fields if self.object else None
+        if 'formset' not in context:
+            formset = FieldFormSet(prefix='field', initial=initial)
+            formset.extra = 0 if initial else 1
+            context.update({
+                'formset': formset
+            })
+        return context
+
     def post(self, request, *args, **kwargs):
-        self.object = None
+        self.object = self.get_object()
         formset = FieldFormSet(self.request.POST, prefix='field')
         form_kwargs = self.get_form_kwargs()
         data = form_kwargs['data'].copy()
@@ -44,3 +36,16 @@ class FormCreateView(generic.CreateView):
         return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 
+class FormsListView(generic.ListView):
+    model = Forms
+
+
+class FormUpdateView(DynamicFormMixin, generic.UpdateView):
+    form_class = FormsCreateForm
+    model = Forms
+    template_name = 'forms_app/forms_edit.html'
+
+
+class FormCreateView(DynamicFormMixin, generic.CreateView):
+    template_name = 'forms_app/form_create.html'
+    form_class = FormsCreateForm
